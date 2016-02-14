74b2cf6a02526f8b353c5d46720c719123878b13
Hanchen Wang <hanchen.wang@mail.utoronto.ca>
2016-02-13 22:10:11 -0500

    CONR-23: Changed view controller names

--- Constant Reminder/Constant Reminder/AddTaskViewController.swift
+++ Constant Reminder/Constant Reminder/AddTaskViewController.swift
@@ -1,25 +1,30 @@
 //
-//  ViewController.swift
+//  AddTaskViewController.swift
 //  Constant Reminder
 //
-//  Created by Hanchen Wang on 2016-01-25.
+//  Created by Hanchen Wang on 2016-02-13.
 //  Copyright © 2016 Asian Social Rejects. All rights reserved.
 //
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class AddTaskViewController: UIViewController {
+    
+    @IBOutlet weak var title_text: UITextField!
+    @IBOutlet weak var desc_text: UITextField!
+    @IBOutlet weak var prority: UISegmentedControl!
+    @IBOutlet weak var create: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
--- Constant Reminder/Constant Reminder/MainViewController.swift
+++ Constant Reminder/Constant Reminder/MainViewController.swift
@@ -1,31 +1,25 @@
 //
-//  SecondViewController.swift
+//  ViewController.swift
 //  Constant Reminder
 //
-//  Created by TIANCHEN GU on 2016-02-02.
+//  Created by Hanchen Wang on 2016-01-25.
 //  Copyright © 2016 Asian Social Rejects. All rights reserved.
 //
 
-
 import UIKit
 
-class SecondViewController: UIViewController {
-    
-    @IBOutlet weak var title_text: UITextField!
-    @IBOutlet weak var desc_text: UITextField!
-    @IBOutlet weak var prority: UISegmentedControl!
-    @IBOutlet weak var create: UIButton!
-    
+class MainViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
+
+
 }
+
